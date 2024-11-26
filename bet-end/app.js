1d9eaa425d2bd4ce1832f2c59eee7cdb55e5198f
messex04 messex04 <messina.daniele.2004@gmail.com>
2024-11-26 12:00:50 +0000

    punto 4 finito

--- bet-end/app.js
+++ bet-end/app.js
@@ -1,17 +1,17 @@
-// app.js
+
 const express = require('express');
 const app = express();
 const port = 3000;
 
-// Serve file statici dalla cartella 'www'
+
 app.use(express.static('www'));
 
-// Definisci l'endpoint /api/mante per restituire il file mante.json
+
 app.get('/api/mante', (req, res) => {
   res.json(require('./mante.json'));
 });
 
-// Avvia il server
+
 app.listen(port, () => {
   console.log(`Server in ascolto sulla porta ${port}`);
 });
