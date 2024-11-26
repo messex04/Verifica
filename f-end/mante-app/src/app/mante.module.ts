30ea7b13cfce35cceff7fc28d744071ef77a23b8
messex04 messex04 <messina.daniele.2004@gmail.com>
2024-11-26 12:47:45 +0000

    punto 7

---
+++ f-end/mante-app/src/app/mante.module.ts
@@ -0,0 +1,10 @@
+export interface Mante {
+    _id: string;
+    estinzione: boolean;
+    apertura_alare: string;
+    immagine: string;
+    eta: number;
+    colore: string;
+    nome_specie: string;
+    nome_razza: string;
+  }
